7f6d729e150b89545a85bc5a8e40e887abe88f38
charles2alvin <yggg100@126.com>
2020-11-29 22:21:39 +0100

    Problem 1 can run now

--- problem_2.py
+++ problem_2.py
@@ -1,28 +1,13 @@
 # Xitao Mo 970919-4691
 # Jingning Zhou 970413-6705
 
-import maze_rob as mr
 import matplotlib.pyplot as plt
 import numpy as np
 import rob_bank as rb
 
-city = mr.City()
+
 gammas = [0.1, 0.3, 0.5, 0.7, 0.9]
 Vs = []
-
-for gamma in gammas:
-    print(gamma)
-    epsilon = 0.0001
-    V, policy = mr.value_iteration(city, gamma, epsilon)
-    idx = city.map_[city.start_state]
-    Vs.append(V[idx])
-    city.draw(policy, plot=True, arrows=True)
-
-plt.plot(gammas, Vs, color='green')
-plt.xlabel("Discount factor")
-plt.ylabel("Value Function")
-plt.show()
-path = city.simulate(city.start_state, policy, method="ValIter", survival_factor=gamma)
 
 
 def run():
@@ -37,12 +22,19 @@ def run():
     # 1 = bank
     # 2 = police station
 
-    method = 'DynProg'
-    start = (0, 0, 1, 2)
-
     env = rb.Maze(maze)
-    V, policy = rb.dynamic_programming(env)
+    gamma = 0.4
+    epsilon = 0.0001
+    V, policy = rb.value_iteration(env, gamma, epsilon)
+
+    method = 'ValIter'
+    start = (0, 0, 1, 2)
+    path = env.simulate(start, policy, method)
 
     env.simulate(start, policy, method)
+
+    rb.animate_solution(maze, path)
+
+    env.draw_optimal_policy(maze, policy)
 
 
